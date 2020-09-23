8ea6f1b4e986e322124af38e4b04bd3d93bd16da
akine <weezer.and.you@gmail.com>
2020-09-23 09:26:20 +0900

    modi async log

--- app/jobs/async_log_job.rb
+++ app/jobs/async_log_job.rb
@@ -1,7 +1,7 @@
 class AsyncLogJob < ApplicationJob
   queue_as :default
 
-  def perform(*args)
-    # Do something later
+  def perform(message: "hello") # message引数を追加
+    AsyncLog.create!(message: message) # DBに保存する
   end
 end
